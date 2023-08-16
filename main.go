56dffc834ea9fdafdcd8d56eec8e0eca5eacbfce
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2023-08-16 19:06:00 -0400

    XHR with code preview

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"embed"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"strings"
@@ -15,8 +16,15 @@ import (
 //go:embed www/*.js
 var www embed.FS
 
+type codeRepo interface {
+	Store(id, code string) error
+	Load(id string) (string, error)
+}
+
 type server struct {
 	indexHtml *template.Template
+	appHtml   *template.Template
+	codeRepo  codeRepo
 }
 
 func (s *server) getRoot(w http.ResponseWriter, r *http.Request) {
@@ -41,6 +49,57 @@ func (s *server) getW3(w http.ResponseWriter, r *http.Request) {
 	w.Write(file)
 }
 
+func (s *server) postUpdate(w http.ResponseWriter, r *http.Request) {
+	code, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "SERVER ERROR")
+		return
+	}
+	if err := s.codeRepo.Store("thecode", string(code)); err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "SERVER ERROR")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status": "OK"}`))
+}
+
+func (s *server) getPreview(w http.ResponseWriter, r *http.Request) {
+	code, err := s.codeRepo.Load("thecode")
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "SERVER ERROR")
+		return
+	}
+	if err := s.appHtml.ExecuteTemplate(w, "app.html", struct {
+		Code string
+	}{
+		Code: code,
+	}); err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "SERVER ERROR")
+		return
+	}
+}
+
+type inMemCodeStore struct {
+	codes map[string]string
+}
+
+func (i *inMemCodeStore) Load(id string) (string, error) {
+	if c, ok := i.codes[id]; ok {
+		return c, nil
+	}
+	return "", fmt.Errorf("NOT FOUND")
+}
+
+func (i *inMemCodeStore) Store(id, code string) error {
+	i.codes[id] = code
+	return nil
+}
+
 func main() {
 	f, err := www.ReadFile("www/index.html")
 	if err != nil {
@@ -50,8 +109,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	s := &server{t}
+	f1, err := www.ReadFile("www/app.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	t1, err := template.New("app.html").Parse(string(f1))
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := &server{
+		indexHtml: t,
+		appHtml:   t1,
+		codeRepo: &inMemCodeStore{
+			codes: map[string]string{
+				"thecode": "// TODO",
+			},
+		},
+	}
 	http.HandleFunc("/www/", s.getW3)
+	http.HandleFunc("/preview/", s.getPreview)
+	http.HandleFunc("/update/", s.postUpdate)
 	http.HandleFunc("/", s.getRoot)
 	if err := http.ListenAndServe(":3333", nil); err != nil {
 		log.Fatal(err)
--- www/livecode.js
+++ www/livecode.js
@@ -1,4 +1,27 @@
-var myCodeMirror = CodeMirror(document.body, {
+var myCodeMirror = CodeMirror(document.getElementById("editor"), {
   value: "function myScript(){return 100;}\n",
   mode:  "javascript"
 });
+
+myCodeMirror.setSize(null /*retain width*/, window.innerHeight-50);
+document.getElementById("previewFrame").style.height = (window.innerHeight-50) + "px";
+
+async function update() {
+    const response = await fetch("/update/", {
+        method: "POST",
+        mode: "same-origin",
+        cache: "no-cache",
+        headers: {"Content-Type": "text/plain"},
+        body: myCodeMirror.getValue(),
+    });
+    return response.json();
+}
+
+function save() {
+    console.log("Saving");
+    update().then((resp) => {
+        console.log("Saved", resp);
+    });
+}
+
+CodeMirror.keyMap.default['Ctrl-S'] = save;
