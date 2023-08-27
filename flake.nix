03cea71ae39b3cdd479586115eaae7a095e89b4a
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2023-08-26 22:29:43 -0400

    With NIXOS Module

--- flake.nix
+++ flake.nix
@@ -31,34 +31,72 @@
         sha256 = "1fmrq3cqca4y2dvyrgzc86avjphh5rpw1mjwzx226bnfp4a8yzxv";
       };
 
-      package = { system }:
+      fullSources = pkgs:
+        pkgs.stdenv.mkDerivation {
+          name = "t0-livecode-sources-${ver}";
+          src = ./.;
+          buildPhase = ''
+            mkdir -p $out
+            cp -r $src/*.go $src/*.mod $src/*.sum $out/
+            mkdir -p $out/www
+            cp ${cmCSS} $out/www/codemirror.min.css
+            cp ${cmJS} $out/www/codemirror.min.js
+            cp ${cmModeJS} $out/www/javascript.min.js
+            cp ${p5JS} $out/www/p5.min.js
+            cp -r $src/www/* $out/www/
+          '';
+        };
+
+      app = pkgs:
+        pkgs.buildGo120Module rec {
+          name = "t0-livecode-${ver}";
+          version = "${ver}";
+          src = fullSources pkgs;
+          vendorSha256 = null;
+        };
+
+      package = system:
         let
           pkgs = import nixpkgs { system = system; };
-          fullSources = pkgs.stdenv.mkDerivation {
-            name = "t0-livecode-sources-${ver}";
-            src = ./.;
-            buildPhase = ''
-              mkdir -p $out
-              cp -r $src/*.go $src/*.mod $src/*.sum $out/
-              mkdir -p $out/www
-              cp ${cmCSS} $out/www/codemirror.min.css
-              cp ${cmJS} $out/www/codemirror.min.js
-              cp ${cmModeJS} $out/www/javascript.min.js
-              cp ${p5JS} $out/www/p5.min.js
-              cp -r $src/www/* $out/www/
-            '';
-          };
         in
-          pkgs.buildGo120Module rec {
-            name = "t0-livecode-${ver}";
-            version = "${ver}";
-            src = fullSources;
-            vendorSha256 = null;
-          };
+          app pkgs;
     in
       {
-        packages.x86_64-linux.default = package {
-          system = "x86_64-linux";
-        };
+        packages.x86_64-linux.default = package "x86_64-linux";
+
+        nixosModules.t0-livecode-module = { config, pkgs, ... }:
+          with nixpkgs.lib;
+          let
+            cfg = config.services.t0-livecode;
+            p = app pkgs;
+          in
+            {
+              imports = [];
+              options = {
+                servies.t0-livecode = {
+                  enable = mkEnableOption "t0-livecode";
+                  environment-file = mkOption {
+                    description = "Root-readable env file, can store secrets";
+                    example = "/etc/t0-livecode.conf";
+                  };
+                };
+              };
+              config = mkIf cfg.enable {
+                systemd.services.t0-livecode = {
+                  after = [ "network-online.target" ];
+                  before = [ "multi-user.target" ];
+                  wantedBy = [ "multi-user.target" ];
+                  serviceConfig = {
+                    DynamicUser = true;
+                    # App can read this value with $STATE_DIRECTORY
+                    StateDirectory = "t0-livecode-state";
+                    ExecStart = "${p}/bin/t0-livecode";
+                    ProtectSystem = "strict";
+                    ProtectHome = true;
+                    EnvironmentFile = cfg.environment-file;
+                  };
+                };
+              };
+            };
       };
 }
