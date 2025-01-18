e4d01e68cf2c3985d737c1bd03176e81cadcebe0
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2025-01-17 21:25:37 -0500

    Updates

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A flake for t0-livecode, including a Nixos service module";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-24.11;
   };
 
   outputs = { self, nixpkgs }:
