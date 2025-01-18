73b20eb241fcee6aaba22160da1851484cade53f
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2025-01-17 23:11:00 -0500

    chore: reduce flake indent

--- flake.nix
+++ flake.nix
@@ -5,37 +5,38 @@
     nixpkgs.url = github:NixOS/nixpkgs/nixos-24.11;
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      ver = "0.0.1";
-      p5 = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.7.0/";
-      codeMirror = "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7";
+  outputs = { self, nixpkgs }: let
 
-      cmCSS = builtins.fetchurl {
-        url = "${codeMirror}/codemirror.min.css";
-        sha256 = "1i2zr35sihmfw9sp832p1bqa5m67cliwd18hwjgx4mb9mc9721qi";
-      };
+    ver = "0.0.1";
 
-      cmJS = builtins.fetchurl {
-        url = "${codeMirror}/codemirror.min.js";
-        sha256 = "1in7flnhkaarq9qqihyc8bxz5ynvdrgfzmn4cgivj41f3v78k1j5";
-      };
+    p5 = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.7.0/";
+    codeMirror = "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7";
 
-      cmModeJS = builtins.fetchurl {
-        url = "${codeMirror}/mode/javascript/javascript.min.js";
-        sha256 = "1462qdkm40ak0x96x0i1ngw1l3l92rc0acdl8lfpgv9aiya0alxk";
-      };
+    cmCSS = builtins.fetchurl {
+      url = "${codeMirror}/codemirror.min.css";
+      sha256 = "1i2zr35sihmfw9sp832p1bqa5m67cliwd18hwjgx4mb9mc9721qi";
+    };
 
-      p5JS = builtins.fetchurl {
-        url = "${p5}/p5.min.js";
-        sha256 = "1fmrq3cqca4y2dvyrgzc86avjphh5rpw1mjwzx226bnfp4a8yzxv";
-      };
+    cmJS = builtins.fetchurl {
+      url = "${codeMirror}/codemirror.min.js";
+      sha256 = "1in7flnhkaarq9qqihyc8bxz5ynvdrgfzmn4cgivj41f3v78k1j5";
+    };
 
-      fullSources = pkgs:
-        pkgs.stdenv.mkDerivation {
-          name = "t0-livecode-sources-${ver}";
-          src = ./.;
-          buildPhase = ''
+    cmModeJS = builtins.fetchurl {
+      url = "${codeMirror}/mode/javascript/javascript.min.js";
+      sha256 = "1462qdkm40ak0x96x0i1ngw1l3l92rc0acdl8lfpgv9aiya0alxk";
+    };
+
+    p5JS = builtins.fetchurl {
+      url = "${p5}/p5.min.js";
+      sha256 = "1fmrq3cqca4y2dvyrgzc86avjphh5rpw1mjwzx226bnfp4a8yzxv";
+    };
+
+    fullSources = pkgs:
+      pkgs.stdenv.mkDerivation {
+        name = "t0-livecode-sources-${ver}";
+        src = ./.;
+        buildPhase = ''
             mkdir -p $out
             cp -r $src/*.go $src/*.mod $src/*.sum $out/
             mkdir -p $out/www
@@ -45,59 +46,50 @@
             cp ${p5JS} $out/www/p5.min.js
             cp -r $src/www/* $out/www/
           '';
-        };
+      };
 
-      app = pkgs:
-        pkgs.buildGo123Module rec {
-          name = "t0-livecode-${ver}";
-          version = "${ver}";
-          src = fullSources pkgs;
-          vendorHash = null;
-        };
+    app = pkgs:
+      pkgs.buildGo123Module rec {
+        name = "t0-livecode-${ver}";
+        version = "${ver}";
+        src = fullSources pkgs;
+        vendorHash = null;
+      };
 
-      package = system:
-        let
-          pkgs = import nixpkgs { system = system; };
-        in
-          app pkgs;
-    in
-      {
-        packages.x86_64-linux.default = package "x86_64-linux";
-        packages.aarch64-darwin.default = package "aarch64-darwin";
+    package = system: let
+      pkgs = import nixpkgs { system = system; };
+    in app pkgs;
+  in {
+    packages.x86_64-linux.default = package "x86_64-linux";
+    packages.aarch64-darwin.default = package "aarch64-darwin";
 
-        nixosModules.t0-livecode-module = { config, pkgs, ... }:
-          with nixpkgs.lib;
-          let
-            cfg = config.services.t0-livecode;
-            p = app pkgs;
-          in
-            {
-              imports = [];
-              options = {
-                services.t0-livecode = {
-                  enable = mkEnableOption "t0-livecode";
-                  # environment-file = mkOption {
-                  #   description = "Root-readable env file, can store secrets";
-                  #   example = "/etc/t0-livecode.conf";
-                  # };
-                };
-              };
-              config = mkIf cfg.enable {
-                systemd.services.t0-livecode = {
-                  after = [ "network-online.target" ];
-                  before = [ "multi-user.target" ];
-                  wantedBy = [ "multi-user.target" ];
-                  serviceConfig = {
-                    DynamicUser = true;
-                    # App can read this value with $STATE_DIRECTORY
-                    StateDirectory = "t0-livecode-state";
-                    ExecStart = "${p}/bin/t0-livecode";
-                    ProtectSystem = "strict";
-                    ProtectHome = true;
-                    # EnvironmentFile = cfg.environment-file;
-                  };
-                };
+    nixosModules.t0-livecode-module = { config, pkgs, ... }:
+      with nixpkgs.lib; let
+        cfg = config.services.t0-livecode;
+        p = app pkgs;
+      in
+        {
+          imports = [];
+          options = {
+            services.t0-livecode = {
+              enable = mkEnableOption "t0-livecode";
+            };
+          };
+          config = mkIf cfg.enable {
+            systemd.services.t0-livecode = {
+              after = [ "network-online.target" ];
+              before = [ "multi-user.target" ];
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = {
+                DynamicUser = true;
+                # App can read this value with $STATE_DIRECTORY
+                StateDirectory = "t0-livecode-state";
+                ExecStart = "${p}/bin/t0-livecode";
+                ProtectSystem = "strict";
+                ProtectHome = true;
               };
             };
-      };
+          };
+        };
+  };
 }
